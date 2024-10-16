fd533f2bb2c65f7727b8d0f417e611b51860b21b
Kaylee-Baichulall-ICD2O1-01 <kaylee.baichulall@student.tdsb.on.ca>
2024-10-16 14:17:51 +0000

    yeay

--- PP2_3.py
+++ PP2_3.py
@@ -7,7 +7,6 @@ Date Last Modified: Oct 16, 2024
 
 def q1(): 
   word = input("In: ")
-  
   if word [-2:] == "ey":
     print("-eys")
   elif word [-1:] == "y":
@@ -25,14 +24,24 @@ def q2():
   elif num > 0:
     num = str(num)
     print(num + " is positive")
+
+def q3():
+  side1 = float(input("Input a number: "))
+  side2 = float(input("Input a number: "))
+  side3 = float(input("Input a number: "))
+  if (side1 + side2) < side3 or (side1 + side3) < side2 or (side3 + side2) < side1:
+    print("No Triangle")
+  elif side1 == side2 and side2 == side3 and side1 == side3:
+    print("Equilateral")
+  elif side1 == side2 or side1 == side3 or side2 == side3:
+    print("Isosceles")
   else:
-    num = str(num)
-    print(" ")
-  
+    print("Scalene")
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 '''
 q1()
 q2()
+q3()
 '''
