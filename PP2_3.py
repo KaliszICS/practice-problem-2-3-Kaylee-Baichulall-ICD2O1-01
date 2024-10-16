25292fa717c8657b12e0edf7fbb35e28a588c628
Kaylee-Baichulall-ICD2O1-01 <kaylee.baichulall@student.tdsb.on.ca>
2024-10-16 13:57:05 +0000

    whaa

--- PP2_3.py
+++ PP2_3.py
@@ -1,16 +1,34 @@
-
+'''
+Lesson: Else if
+Author: Kaylee Baichulall
+Date Created: Oct 16, 2024
+Date Last Modified: Oct 16, 2024
+'''
 
 def q1(): 
-  #Write Assignment code here
-
+  word = input("In: ")
+  if word [-1] == "y":
+    print("-ies")
+  elif word [-2:0] == "ey":
+    print("-eys")
+  elif word [-3:0] == "ife":
+    print("-ives")
+  else:
+    print("-s")
 
 def q2(): 
-  #Write Assignment code here
-
-
+  num = int(input("In: "))
+  if num < 0:
+    print(num + " is negative")
+  elif num > 0:
+    print(num + " is positive")
+  else:
+    print(" ")
+  
 
 #Do not alter the following code
 #Comment out the following code when running your tests
-
+'''
 q1()
 q2()
+'''
