500d03e7dd540ec4f5f9c58acab5d9031d50801a
Kaylee-Baichulall-ICD2O1-01 <kaylee.baichulall@student.tdsb.on.ca>
2024-10-16 14:03:56 +0000

    huh

--- PP2_3.py
+++ PP2_3.py
@@ -7,11 +7,12 @@ Date Last Modified: Oct 16, 2024
 
 def q1(): 
   word = input("In: ")
-  if word [-1] == "y":
-    print("-ies")
-  elif word [-2:0] == "ey":
+  
+  if word [-2:] == "ey":
     print("-eys")
-  elif word [-3:0] == "ife":
+  elif word [-1:] == "y":
+    print("-ies")
+  elif word [-3:] == "ife":
     print("-ives")
   else:
     print("-s")
@@ -19,10 +20,13 @@ def q1():
 def q2(): 
   num = int(input("In: "))
   if num < 0:
+    num = str(num)
     print(num + " is negative")
   elif num > 0:
+    num = str(num)
     print(num + " is positive")
   else:
+    num = str(num)
     print(" ")
   
 
